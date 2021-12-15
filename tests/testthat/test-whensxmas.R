26fff03fb309c5c123ccd6e2df8cb3c4a04b8d15
RaoulWolf <raoul.wolf@ngi.no>
2021-12-15 17:25:25 +0100

    More tests.

--- tests/testthat/test-whensxmas.R
+++ tests/testthat/test-whensxmas.R
@@ -1,5 +1,15 @@
 test_that("when's Christmas", {
+
   expect_message(
     whensxmas()
     )
+
+  expect_message(
+    whensxmas(date = "2021-01-01")
+  )
+
+  expect_message(
+    whensxmas(date = "2021-12-25")
+  )
+
 })
